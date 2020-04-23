86e3de2a16a7e69c2ba1449fa9a1b698c1bc355b
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-04-23 15:03:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -168,6 +168,12 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
+  window.requestAnimationFrame(function(){
+    var left = positionToInteger(DODGER.style.left)
+    if (left < 360) {
+    dodger.style.left = `${left - 4}px`
+  }
+  })
   // implement me!
   /**
    * This function should move DODGER to the right
